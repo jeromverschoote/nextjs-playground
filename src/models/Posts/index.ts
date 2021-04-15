898328fa345c0a946bbdd2486ed9702e23c97b91
Jerom Verschoote <33222896+jeromverschoote@users.noreply.github.com>
2021-04-15 20:55:55 +0200

    :speech_balloon: Update dummy copy for posts

--- src/models/Posts/index.ts
+++ src/models/Posts/index.ts
@@ -4,35 +4,40 @@ const DATA = [
   {
     id: 1,
     title: 'Hello Next.js!',
-    text: 'Today I build my first Next App!',
+    text:
+      'Today I build my first Next App. Halvah jujubes brownie cupcake sugar plum cookie caramels sweet roll donut. Gummi bears tiramisu wafer marzipan powder carrot cake wafer bear claw lemon drops. Brownie tart jelly danish dragée pudding sesame snaps carrot cake. Carrot cake caramels tart.',
     date: '',
     authorId: 1,
   },
   {
     id: 2,
-    title: 'Wolla!',
-    text: 'Today I build my first Next App!',
+    title: 'Hello Flower!',
+    text:
+      'Today I picked my first flower. Halvah jujubes brownie cupcake sugar plum cookie caramels sweet roll donut. Gummi bears tiramisu wafer marzipan powder carrot cake wafer bear claw lemon drops. Brownie tart jelly danish dragée pudding sesame snaps carrot cake. Carrot cake caramels tart.',
     date: '',
     authorId: 2,
   },
   {
     id: 3,
-    title: 'Hello Next.js!',
-    text: 'Today I build my first Next App!',
+    title: 'Hello Pokemon!',
+    text:
+      'Today I caught my first Pokemon. Halvah jujubes brownie cupcake sugar plum cookie caramels sweet roll donut. Gummi bears tiramisu wafer marzipan powder carrot cake wafer bear claw lemon drops. Brownie tart jelly danish dragée pudding sesame snaps carrot cake. Carrot cake caramels tart.',
     date: '',
     authorId: 3,
   },
   {
     id: 4,
-    title: 'Hello Next.js!',
-    text: 'Today I build my first Next App!',
+    title: 'Hello Smartphone!',
+    text:
+      'Today I repaired my first smartphone. Halvah jujubes brownie cupcake sugar plum cookie caramels sweet roll donut. Gummi bears tiramisu wafer marzipan powder carrot cake wafer bear claw lemon drops. Brownie tart jelly danish dragée pudding sesame snaps carrot cake. Carrot cake caramels tart.',
     date: '',
     authorId: 4,
   },
   {
     id: 5,
-    title: 'Hello Next.js!',
-    text: 'Today I build my first Next App!',
+    title: 'Hello Bitcoin!',
+    text:
+      'Today I bought my first Bitcoin. Halvah jujubes brownie cupcake sugar plum cookie caramels sweet roll donut. Gummi bears tiramisu wafer marzipan powder carrot cake wafer bear claw lemon drops. Brownie tart jelly danish dragée pudding sesame snaps carrot cake. Carrot cake caramels tart.',
     date: '',
     authorId: 5,
   },
