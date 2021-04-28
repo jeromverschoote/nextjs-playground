84e0c3666845824a3cdaedaf648d1f78bc5bf1c9
Jerom Verschoote <33222896+jeromverschoote@users.noreply.github.com>
2021-04-28 16:55:32 +0200

    :lipstick: Remove top padding on forms

--- src/views/Login/styles.ts
+++ src/views/Login/styles.ts
@@ -1,14 +1,9 @@
-import { breakpoints } from 'src/styles';
 import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 1rem;
   max-width: 30rem;
   margin: 0 auto;
-
-  @media (min-width: ${breakpoints.md}) {
-    padding-top: 3.5rem;
-  }
 `;
 
 export const Title = styled.h2`
